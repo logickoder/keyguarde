530537aa6734cec675dbf1f6b45951e27b4d0ebd
Jeffery Orazulike <chukwudumebiorazulike@gmail.com>
2025-05-12 23:57:17 +0100

    hide banner ad when not loaded
    Signed-off-by: Jeffery Orazulike <chukwudumebiorazulike@gmail.com>

--- app/src/main/java/dev/logickoder/keyguarde/app/components/BannerAd.kt
+++ app/src/main/java/dev/logickoder/keyguarde/app/components/BannerAd.kt
@@ -1,29 +1,49 @@
 package dev.logickoder.keyguarde.app.components
 
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.viewinterop.AndroidView
+import com.google.android.gms.ads.AdListener
 import com.google.android.gms.ads.AdRequest
 import com.google.android.gms.ads.AdSize
 import com.google.android.gms.ads.AdView
+import com.google.android.gms.ads.LoadAdError
 import dev.logickoder.keyguarde.BuildConfig
 
 @Composable
 fun BannerAd(
     modifier: Modifier = Modifier,
 ) {
-    AndroidView(
-        modifier = modifier.fillMaxWidth(),
-        factory = { context ->
-            AdView(context).apply {
-                setAdSize(AdSize.BANNER)
-                adUnitId = when (BuildConfig.DEBUG) {
-                    true -> "ca-app-pub-3940256099942544/9214589741"
-                    else -> "ca-app-pub-9535789616988908/5814771713"
+    var isAdLoaded by remember { mutableStateOf(false) }
+
+    AnimatedVisibility(visible = isAdLoaded) {
+        AndroidView(
+            modifier = modifier.fillMaxWidth(),
+            factory = { context ->
+                AdView(context).apply {
+                    setAdSize(AdSize.BANNER)
+                    adUnitId = when (BuildConfig.DEBUG) {
+                        true -> "ca-app-pub-3940256099942544/9214589741"
+                        else -> "ca-app-pub-9535789616988908/5814771713"
+                    }
+                    adListener = object : AdListener() {
+                        override fun onAdLoaded() {
+                            isAdLoaded = true
+                        }
+
+                        override fun onAdFailedToLoad(error: LoadAdError) {
+                            isAdLoaded = false
+                        }
+                    }
+                    loadAd(AdRequest.Builder().build())
                 }
-                loadAd(AdRequest.Builder().build())
             }
-        }
-    )
+        )
+    }
 }
