12df657a3bbe5d73ed0fe914dddbcedbaf6560f5
Jeffery Orazulike <chukwudumebiorazulike@gmail.com>
2025-05-16 12:48:00 +0100

    chore(ci/cd): add closed testing track
    Signed-off-by: Jeffery Orazulike <chukwudumebiorazulike@gmail.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,7 +37,7 @@ android {
         applicationId = "dev.logickoder.keyguarde"
         minSdk = 26
         targetSdk = 36
-        versionCode = 4
+        versionCode = 6
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
