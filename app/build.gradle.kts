d1f7b28335016d20b8f1330b3ffdb29faa5061d9
Jeffery Orazulike <chukwudumebiorazulike@gmail.com>
2025-06-29 14:25:18 +0100

    feat(version): increment versionCode to 9 for upcoming release
    Signed-off-by: Jeffery Orazulike <chukwudumebiorazulike@gmail.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,7 +37,7 @@ android {
         applicationId = "dev.logickoder.keyguarde"
         minSdk = 26
         targetSdk = 36
-        versionCode = 8
+        versionCode = 9
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
